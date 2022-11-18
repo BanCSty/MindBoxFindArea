00812aa87d68b5286135f068d080c179df637554
Rust <garenrequest@gmail.com>
2022-11-18 22:19:20 +0300

    2.2 Complete class Triangle

--- MinBoxFindArea/Shapes/Triangle.cs
+++ MinBoxFindArea/Shapes/Triangle.cs
@@ -1,28 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace MinBoxFindArea.Shapes
 {
     public class Triangle : Figure
     {
-        
+
         private double _a, _b, _c;
-       
-        public double A => _a;
-        public double B => _b;
-        public double C => _c;
+
+        public double A { get => _a; set => _a = CeckValue(value); }
+        public double B { get => _b; set => _b = CeckValue(value); }
+        public double C { get => _c; set => _c = CeckValue(value); }
 
         public Triangle(double a, double b, double c)
         {
-            _a = a;
-            _b = b;
-            _c = c;
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public bool IsRectengular()
+        {
+            double max = new double[] { A, B, C }.Max();
+            double maxPow = max * max * 2;
+            return maxPow == A * A + B * B + C * C;
         }
 
         public override double GetArea()
         {
-            throw new NotImplementedException();
+            var halfPerimeter = (A + B + C) / 2;
+
+            //Coefficient
+            var ACoff = halfPerimeter - A;
+            var BCoff = halfPerimeter - B;
+            var CCoff = halfPerimeter - C;
+
+            return Math.Sqrt(halfPerimeter * ACoff * BCoff * CCoff);
+        }
+
+        public double CeckValue(double value)
+        {
+            if (value >= 0)
+                return value;
+            else throw new ArgumentException("Value must be positiv :)");
         }
     }
 }
