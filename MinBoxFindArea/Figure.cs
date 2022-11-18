323d652b836a99872de878c7700cc39484ff3b1c
Rust <garenrequest@gmail.com>
2022-11-18 22:12:12 +0300

    2. completed class Figure

--- MinBoxFindArea/Figure.cs
+++ MinBoxFindArea/Figure.cs
@@ -4,6 +4,15 @@ namespace MinBoxFindArea
 {
     public abstract class Figure
     {
+        private readonly Lazy<double> _area;
+
+        public double Area => _area.Value;
+
+        protected Figure()
+        {
+            _area = new Lazy<double>(GetArea);
+        }
+
         public abstract double GetArea();
     }
 }
